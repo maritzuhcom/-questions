a5730f543da4c0552c8f45500ffcc52fbf4b48c8
Maritza Mancillas <mancillas.maritza@gmail.com>
2019-03-22 20:07:08 -0700

    maxChar

--- Algos/mostRepeatingChar.js
+++ Algos/mostRepeatingChar.js
@@ -1,2 +1,28 @@
 // find most repeating char
-const strEx = 'abcabcbb';
+
+const maxChar = (arr) => {
+  let newArr = arr[0].split('');
+  let obj = {};
+
+  for (let i = 0; i < newArr.length; i += 1) {
+    if (!obj[newArr[i]]) {
+      obj[newArr[i]] = 1;
+    } else {
+      obj[newArr[i]] += 1;
+    }
+  }
+
+  let max = -Infinity;
+  let maxCharacter = '';
+
+  for (key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxCharacter = key;
+    }
+  }
+  return maxCharacter;
+};
+
+console.log(maxChar(['Maritza']));
+console.log(maxChar(['abcabcbb']));
